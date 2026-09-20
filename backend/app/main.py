fde195c21357e0cdc01bf28d5c19f613339f958e
Adeel Umar <adeelumar30@hotmail.com>
2026-09-20 09:47:59 +0500

    update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -60,13 +60,7 @@ app = FastAPI(
     lifespan=lifespan,
 )
 
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+
 
 app.include_router(api_router)
 
